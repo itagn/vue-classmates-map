937e296296c2d0519409169791b5056a0f980c47
caidong <2361305184@qq.com>
2018-07-23 17:40:54 +0800

    添加示例组件

---
+++ demo/demo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <classmates-map class="map" :pData="pData"></classmates-map>
+  </div>
+</template>
+<script>
+import json from '../demo.json'
+import { ClassmatesMap } from 'vue-classmates-map'
+export default {
+  components: {
+    ClassmatesMap
+  },
+  data () {
+    return {
+      pData: {
+        data: json.data,
+        node: {
+          className: '.demo1'
+        }
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.map {
+  width: 600px;
+  height: 300px;
+}
+</style>
